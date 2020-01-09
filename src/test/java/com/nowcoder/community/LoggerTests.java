31c5eb832f7c6d32b6d2bc95f9b2f260389ff701
Mosand <974386121@qq.com>
2020-01-09 16:22:40 +0800

    首次提交

---
+++ src/test/java/com/nowcoder/community/LoggerTests.java
@@ -0,0 +1,38 @@
+package com.nowcoder.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class LoggerTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(LoggerTests.class);
+    private static final Logger logger2 = LoggerFactory.getLogger(LoggerTests.class);
+
+    @Test
+    public void testLogger() {
+        System.out.println(logger.getName());
+
+        logger.debug("debug log");
+        logger.info("info log");
+        logger.warn("warn log");
+        logger.error("error log");
+    }
+
+    @Test
+    public void testLogger2() {
+        System.out.println(logger2.getName());
+
+        logger2.debug("debug log2");
+        logger2.info("info log2");
+        logger2.warn("warn log2");
+        logger2.error("error log2");
+    }
+}
